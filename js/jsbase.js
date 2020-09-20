617bb03e8b1339c999745814fd0b928c7436e077
Guilherme Pizzatto <guilherme.pizzatto.shawpatty@gmail.com>
2020-09-20 14:12:54 -0300

    pagina principal concluida, iniciando paginas secundarias

--- js/jsbase.js
+++ js/jsbase.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
 
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+$('.carousel.carousel-slider').carousel({
+   fullWidth: true,
+   indicators: true
+ });
